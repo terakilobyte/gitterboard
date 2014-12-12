fc733bf5b6ef8d9cba16e04acf8dc9071ff97a49
Brian Ball <ideabrian@gmail.com>
2014-12-12 03:07:47 -0800

    Add helpers and bootstrap

--- gitterboard.js
+++ gitterboard.js
@@ -1,1 +1,29 @@
 CampersList = new Mongo.Collection('campers');
+
+if(Meteor.isClient){
+	Template.gitterboard.helpers({
+		'camper': function(){
+			return CampersList.find();
+		},
+		'counter': function(){
+			return CampersList.find().count();
+		}
+	});
+	
+	Template.cambodia.helpers({
+		'camper2': function(){
+		   return "My intuition kicked in!";
+		}
+	});
+
+	Template.copyright.helpers({
+		'copyright': function() {
+			return "copyleft 2015 no rights reserved."
+		}
+	});
+	
+}
+
+if(Meteor.isServer){
+
+}
